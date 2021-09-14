7a712fd3cf7b822cd09f4bb17d557d67e6ecc3c2
Neho <72770312+Starplayer39@users.noreply.github.com>
2021-09-14 23:16:29 +0900

    Update Utility.cs
    2021-09-14 Tue 11:16 P.M
    
    Edit the 'Utility' Script

--- UnityBulletTime/Assets/Scripts/Utility/Utility.cs
+++ UnityBulletTime/Assets/Scripts/Utility/Utility.cs
@@ -4,17 +4,17 @@ public class Utility : MonoBehaviour
 {
     public static bool IsNearlySame(float value, float valueToCompare, float tolerance=0.0001f)
     {
-        float min = valueToCompare - factor;
-        float max = valueToCompare + factor;
+        float min = valueToCompare - tolerance;
+        float max = valueToCompare + tolerance;
 
         return min <= value && value <= max;
     }
 
     public static bool IsNearlySame(Vector3 value, Vector3 valueToCompare, float tolerance=0.0001f)
     {
-        bool b1 = IsNearlySame(value.x, valueToCompare.x, factor);
-        bool b2 = IsNearlySame(value.y, valueToCompare.y, factor);
-        bool b3 = IsNearlySame(value.z, valueToCompare.z, factor);
+        bool b1 = IsNearlySame(value.x, valueToCompare.x, tolerance);
+        bool b2 = IsNearlySame(value.y, valueToCompare.y, tolerance);
+        bool b3 = IsNearlySame(value.z, valueToCompare.z, tolerance);
 
         return b1 && b2 && b3;
     }
