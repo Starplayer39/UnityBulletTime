1f3b99cb64f80177fedb0cd9de282173300c4413
Neho <72770312+Starplayer39@users.noreply.github.com>
2022-02-10 23:54:26 +0900

    Implement Camera Following in BulletTimeTpsCamera.cs
    2022-02-10 Thu 11:54 P.M
    
    Implement Camera Following in BulletTimeTpsCamera.cs

--- UnityBulletTime/Assets/Scripts/Camera/BulletTimeTpsCamera.cs
+++ UnityBulletTime/Assets/Scripts/Camera/BulletTimeTpsCamera.cs
@@ -7,13 +7,16 @@ namespace UnityBulletTime.Camera
     [DisallowMultipleComponent]
     public class BulletTimeTpsCamera : BulletTimeCameraBase
     {
-        [Header("Tps Camera")]
-        [SerializeField] [InspectorName("Aim Target")] protected Transform m_aimTarget;
+        [Header("Tps Camera")]        
+        [SerializeField] [InspectorName("Initial Offset")] protected Vector3 m_initialOffset = new Vector3(45.0f, 0.0f, 0.0f);
         [SerializeField] [InspectorName("Camera Arm Length")] [Range(0.0f, 100.0f)] protected float m_cameraArmLength;
         [SerializeField] [InspectorName("Should Hide Cursor")] protected bool m_shouldHideCursor = true;
         [SerializeField] [InspectorName("Mouse Sensitivity")] [Range(0.0f, 1000.0f)] [BulletTimeVariable] protected float m_mouseSensitivity = 100.0f;
         [SerializeField] [InspectorName("Look Maximum X")] protected float m_lookMaximumX = 50.0f;
         [SerializeField] [InspectorName("Look Minimum X")] protected float m_lookMinimumX = -50.0f;
+        [SerializeField] [InspectorName("Distance")] protected float m_distance = 5.0f;
+
+        protected Vector3 m_orbitAngles;
 
         protected override void Start()
         {
@@ -22,7 +25,7 @@ namespace UnityBulletTime.Camera
             Init();
         }
 
-        private void Update()
+        private void LateUpdate()
         {
             Follow();
         }
@@ -35,7 +38,7 @@ namespace UnityBulletTime.Camera
             {
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
-            }
+            }            
         }
 
         private void OnValidate()
@@ -49,11 +52,24 @@ namespace UnityBulletTime.Camera
             }
 
             m_owner = gameObject.transform.parent?.gameObject;
+            m_orbitAngles = m_initialOffset;
+
+            UpdatePosition();
         }
 
         public override void Follow()
-        {            
+        {
+            UpdatePosition();
         }        
+
+        protected void UpdatePosition()
+        {
+            Vector3 ownerPosition = m_owner.transform.position;
+            Quaternion aimingRotation = Quaternion.Euler(m_orbitAngles);
+            Vector3 aimingDirection = aimingRotation * Vector3.forward;
+            Vector3 aimingPosition = ownerPosition - aimingDirection * m_distance;
+            transform.SetPositionAndRotation(aimingPosition, aimingRotation);
+        }
 
         public override void Look(float mouseX, float mouseY)
         {          
